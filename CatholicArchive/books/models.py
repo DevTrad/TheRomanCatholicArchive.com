8fba8bb5f4f6416e97937796fd881d111928f7b4
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2018-02-10 17:47:53 -0500

    added database migrations, set up admin dashboard

--- CatholicArchive/books/models.py
+++ CatholicArchive/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publish_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.title
+
