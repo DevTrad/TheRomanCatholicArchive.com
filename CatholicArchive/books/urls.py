0570ed02472425abfe35dea894f94ed8100d8a91
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2018-02-10 19:05:09 -0500

    stubbed out main page view

--- CatholicArchive/books/urls.py
+++ CatholicArchive/books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:book_id>/', views.detail, name='detail')
 ]
--- CatholicArchive/books/views.py
+++ CatholicArchive/books/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 def index(request):
-    return HttpResponse('Books index')
+    recently_added_books = Book.objects.order_by('-id')[:10]
 
+    view_model = {
+        'recently_added_books': recently_added_books
+    }
+
+    return render(request, 'books/index.html', view_model)
+
+
+def detail(request):
+    return HttpResponse('Detail view')
