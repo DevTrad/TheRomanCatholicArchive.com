3b25ca1597dc788aa94043660cd613c20f4f29d5
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2018-02-13 22:01:22 -0500

    added book categories

--- CatholicArchive/books/admin.py
+++ CatholicArchive/books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Category
 
 admin.site.register(Book)
+admin.site.register(Category)
+
--- CatholicArchive/books/models.py
+++ CatholicArchive/books/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     download_url = models.CharField(max_length=200)
     publication_year = models.IntegerField('year published', default=0)
     date_created = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.title
